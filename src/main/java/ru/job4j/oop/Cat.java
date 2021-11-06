930258e0860bfffaffacceba70949eb11866ab4c
Eugene Vovk <eugenevovk@gmail.com>
2021-11-07 00:59:34 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,15 +1,42 @@
 package ru.job4j.oop;
 
+/**
+ * Класс, описывающий кота, у нас один, но объектов мы создаем два.
+ * Каждый объект резервирует свой кусочек памяти.
+ * В этот кусок памяти мы записываем данные, которые принадлежат конкретному объекту.
+ */
 public class Cat {
 
-    public String sound() {
-        String voice = "may-may";
-        return voice;
+    private String name;
+    private String food;
+
+    /**
+     * Для того чтобы обратиться к полю объекта, нам нужно использовать ключевое слово this.
+     * Это слово указывает, что нужно вывести содержимое поля.
+     * this. - дальше указываем имя поля, которое нужно вывести.
+     */
+    public void show() {
+        System.out.println(this.name + " ate the " + this.food);
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        System.out.println("There are gav's food");
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food");
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
     }
 }
