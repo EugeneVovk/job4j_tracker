9ab6887a1d980f80fdf7d1d63a6192cece8e09d7
Eugene Vovk <eugenevovk@gmail.com>
2021-11-07 15:24:44 +0300

    2.2. Рефакторинг - Площадь треугольника. [#122642 #225672] added the false test

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -3,6 +3,7 @@ package ru.job4j.oop;
 import static org.hamcrest.Matchers.closeTo;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TriangleTest {
@@ -15,5 +16,16 @@ public class TriangleTest {
         Triangle triangle = new Triangle(a, b, c);
         double rsl = triangle.area();
         assertThat(rsl, closeTo(8, 0.001));
+    }
+
+    @Test
+    public void whenTriangleNotExist() {
+        Point a = new Point(0, 2);
+        Point b = new Point(0, 1);
+        Point c = new Point(0, 7);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        double expected = -1;
+        Assert.assertEquals(expected, rsl, 0.01);
     }
 }
