d6111462d975a038c7d4fbcf9a772e0158a242b5
Eugene Vovk <eugenevovk@gmail.com>
2021-11-07 19:39:56 +0300

    4. Переопределение [#174890] slightly changed the main

--- src/main/java/ru/job4j/inheritance/ReportUsage.java
+++ src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -3,10 +3,13 @@ package ru.job4j.inheritance;
 public class ReportUsage {
     public static void main(String[] args) {
         TextReport report = new TextReport();
-        String text = report.generate("Report's name", "Report's body");
-        System.out.println(text);
         HtmlReport report2 = new HtmlReport();
+        JSONReport report3 = new JSONReport();
+        String text = report.generate("Report's name", "Report's body");
         String html = report2.generate("Report's name", "Report's body");
+        String json = report3.generate("name", "body");
+        System.out.println(text);
         System.out.println(html);
+        System.out.println(json);
     }
 }
