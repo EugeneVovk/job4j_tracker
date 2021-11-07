a5c9f50c1e77f2c42011faf3ab998430f2d34f78
Eugene Vovk <eugenevovk@gmail.com>
2021-11-07 14:56:45 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/main/java/ru/job4j/oop/Point.java
+++ src/main/java/ru/job4j/oop/Point.java
@@ -7,22 +7,37 @@ package ru.job4j.oop;
  * Нам это дает возможность работать с большими объемами кода.
  */
 public class Point {
+    /**
+     * Это поле объекта. Оно доступно только конкретному объекту.
+     */
     private int x;
     private int y;
+    private int z;
 
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+    /**
+     * Конструктор, который принимает начальное состояние объекта "точка"
+     *
+     * @param first  координата x
+     * @param second координата y
+     */
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public Point(int first, int second, int third) {
+        this.x = first;
+        this.y = second;
+        this.z = third;
     }
 
     public double distance(Point that) {
         return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
     }
 
-    public static void main(String[] args) {
-        Point a = new Point(0, 0);
-        Point b = new Point(0, 2);
-        double dist = a.distance(b);
-        System.out.println(dist);
+    public double distance3d(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2)
+                + Math.pow(this.y - that.y, 2)
+                + Math.pow(this.z - that.z, 2));
     }
 }
---
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -0,0 +1,70 @@
+package ru.job4j.oop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void when00And02Then2() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double rsl = a.distance(b);
+        double expected = 2;
+        Assert.assertEquals(expected, rsl, 0.01);
+    }
+
+    @Test
+    public void when12And12Then0() {
+        Point a = new Point(1, 2);
+        Point b = new Point(1, 2);
+        double rsl = a.distance(b);
+        double expected = 0;
+        Assert.assertEquals(expected, rsl, 0.01);
+    }
+
+    @Test
+    public void when12And34Then2dot83() {
+        Point a = new Point(1, 2);
+        Point b = new Point(3, 4);
+        double rsl = a.distance(b);
+        double expected = 2.83;
+        Assert.assertEquals(expected, rsl, 0.01);
+    }
+
+    @Test
+    public void when12AndMinus3Minus4Then7dot21() {
+        Point a = new Point(1, 2);
+        Point b = new Point(-3, -4);
+        double rsl = a.distance(b);
+        double expected = 7.21;
+        Assert.assertEquals(expected, rsl, 0.01);
+    }
+
+    @Test
+    public void when125And342Then5dot74() {
+        Point a = new Point(1, 2, 5);
+        Point b = new Point(3, 4, 0);
+        double rsl = a.distance3d(b);
+        double expected = 5.74;
+        Assert.assertEquals(expected, rsl, 0.01);
+    }
+
+    @Test
+    public void when123AndMinus3Minus40Then7dot81() {
+        Point a = new Point(1, 2, 3);
+        Point b = new Point(-3, -4, 0);
+        double rsl = a.distance3d(b);
+        double expected = 7.81;
+        Assert.assertEquals(expected, rsl, 0.01);
+    }
+
+    @Test
+    public void whenAllValuesAreTheSameThen0() {
+        Point a = new Point(-1, -1, -1);
+        Point b = new Point(-1, -1, -1);
+        double rsl = a.distance3d(b);
+        double expected = 0;
+        Assert.assertEquals(expected, rsl, 0.01);
+    }
+}
