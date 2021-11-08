5475c75eec772fdceb16ab8a0da076b6da778e34
Eugene Vovk <eugenevovk@gmail.com>
2021-11-08 22:16:07 +0300

    3. Перегрузить метод max для трех чисел [#122644 #225684] fixed 'return' according to the mentor's comment

--- src/main/java/ru/job4j/calculator/Max.java
+++ src/main/java/ru/job4j/calculator/Max.java
@@ -3,7 +3,7 @@ package ru.job4j.calculator;
 public class Max {
 
     public static int max(int first, int second) {
-        return Math.max(first, second);
+        return first > second ? first : second;
     }
 
     public static int max(int first, int second, int third) {
