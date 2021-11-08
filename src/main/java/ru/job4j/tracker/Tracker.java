98601dd3014c32af018b628dfe892b0c3cb39760
Eugene Vovk <eugenevovk@gmail.com>
2021-11-08 17:10:13 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,16 +1,49 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
+/**
+ * В классе Tracker должны быть методы:
+ * * добавление заявок - public Item add(Item item);
+ * * получение списка всех заявок - public Item[] findAll();
+ * * получение списка по имени - public Item[] findByName(String key);
+ * * получение заявки по id - public Item findById(int id);
+ * Поле private Item[] items = new Item[100] содержит возможное количество заявлений.
+ * Оно у нас ограничено сотней позиций.
+ */
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
 
+    /**
+     * Метод public Item add(Item item) добавляет заявку,
+     * переданную в аргументах в массив заявок items.
+     * В методе add нужно проставить уникальный ключ в объект Item item.
+     * Это нужно сделать через метод setId.
+     * Поле ids используется для генерации нового ключа.
+     * В нашем примере мы используем последовательность.
+     * То есть каждый вызов метод add будет добавлять в поле ids единицу.
+     * Так мы сможем обеспечить уникальность поле id в Item.
+     * Аналогичный подход используется в базах данных.
+     *
+     * @param item - уникальный ключ.
+     * @return - добавил заявку
+     */
     public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
         return item;
     }
 
+    /**
+     * Метод public Item findById(int id) проверяет в цикле все элементы массива items,
+     * сравнивая id с аргументом int id
+     * и возвращает найденный Item. Если Item не найден - возвращает null.
+     *
+     * @param id - id
+     * @return - возвращает найденный Item. Если Item не найден - возвращает null.
+     */
     public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
@@ -21,5 +54,42 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    /**
+     * Метод public Item[] findAll()
+     *
+     * @return - возвращает копию массива items без null элементов (без пустых ячеек).
+     */
+    public Item[] findAll() {
+        Item[] copy = new Item[size];
+        int len = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                copy[len++] = items[i];
+            }
+        }
+        return Arrays.copyOf(copy, len);
+    }
+
+    /**
+     * Метод public Item[] findByName(String key)
+     * проверяет в цикле все элементы массива items,
+     * сравнивая name (используя метод getName класса Item) с аргументом метода String key.
+     * Элементы, у которых совпадает name, копирует в результирующий массив и возвращает его.
+     * Алгоритм этого метода аналогичен методу findAll.
+     *
+     * @param key - аргументом метода String key
+     * @return - возвращает массив
+     */
+    public Item[] findByName(String key) {
+        Item[] copy = new Item[size];
+        int len = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null && items[i].getName().equals(key)) {
+                copy[len++] = items[i];
+            }
+        }
+        return Arrays.copyOf(copy, len);
     }
 }
