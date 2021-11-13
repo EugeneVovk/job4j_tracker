d3ada539e9cbd074d3c21b05f8a297745e88884c
Eugene Vovk <eugenevovk@gmail.com>
2021-11-14 02:45:56 +0300

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370]

---
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -0,0 +1,24 @@
+package ru.job4j.ex;
+
+/**
+ * java.lang.StackOverflowError - ошибка связана с переполнением области памяти stack.
+ * В ней хранятся ссылки на объекты, локальные переменные и вызовы функций.
+ * Часто можно увидеть при использовании рекурсии.
+ *
+ * Решение проблемы. Поиск логической ошибки в коде.
+ */
+public class FactRec {
+
+    public static int calc(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return calc(n - 1) * n;
+        }
+    }
+
+    public static void main(String[] args) {
+        int rsl = calc(5);
+        System.out.println(rsl);
+    }
+}
