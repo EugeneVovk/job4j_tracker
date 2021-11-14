43dd7d6744f1bb2d0f09c6c829205bcbdbaded08
Eugene Vovk <eugenevovk@gmail.com>
2021-11-14 21:11:53 +0300

    1. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,58 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+/**
+ * Реализовать шаблон singleton.
+ * final, чтобы запретить наследование этого класса.
+ */
+public final class SingleTracker {
+
+    private static SingleTracker instance = null;
+
+    private Tracker tracker = new Tracker();
+
+    /**
+     * Модификатор private.
+     * Таким образом, никто не сможет создать объект этого класса.
+     */
+    private SingleTracker() {
+    }
+
+    /**
+     * Создаём геттер, чтобы иметь ссылку на объект
+     */
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+
+    /**
+     * Чтобы избежать копирование кода используем подход делегирования
+     */
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+}
