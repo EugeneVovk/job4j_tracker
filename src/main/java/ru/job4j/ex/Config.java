c636abd652c6a15876a6fabcaa7642d6b541b5aa
Eugene Vovk <eugenevovk@gmail.com>
2021-11-14 18:27:44 +0300

    3. Замена if-else-throw на if-throw [#49895]

--- src/main/java/ru/job4j/ex/Config.java
+++ src/main/java/ru/job4j/ex/Config.java
@@ -1,13 +1,25 @@
 package ru.job4j.ex;
 
+/**
+ * Здесь используется блок if-else-throw.
+ * Давайте разберем его на части. Здесь производится проверка данных.
+ * Если данные введены верно, то выполнить действие, если нет, то кинуть исключение.
+ * По сути оператор if-else связывает исключение и полезный код.
+ * Такую конструкцию всегда стоит разбивать на две части:
+ * - на блок if-throw
+ * - и полезную часть.
+ */
 public class Config {
+    /**
+     * Первостепенно мы отделили валидацию от полезного кода.
+     * Во вторых код стал меньше и понятнее.
+     */
     public static void load(String path) {
 
-        if (path != null) {
-            System.out.println("Load config by " + path);
-        } else {
+        if (path == null) {
             throw new IllegalArgumentException("Path could not by null.");
         }
+        System.out.println("load config by " + path);
     }
 
     public static void main(String[] args) {
