d60922f5756f6dd5a9ec6e15d2e372cf475edb22
Eugene Vovk <eugenevovk@gmail.com>
2021-11-23 17:05:49 +0300

    2. Преобразования List в Map. [#10093 #230882] added tests

--- src/test/java/ru/job4j/collection/OrderConvertTest.java
+++ src/test/java/ru/job4j/collection/OrderConvertTest.java
@@ -17,4 +17,44 @@ public class OrderConvertTest {
         HashMap<String, Order> map = OrderConvert.process(orders);
         assertThat(map.get("3sfe"), is(new Order("3sfe", "Dress")));
     }
+
+    @Test
+    public void whenSeveralOrders() {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order("3sfe", "Shirt"));
+        orders.add(new Order("5sfe", "Dress"));
+        orders.add(new Order("13sfe", "Jacket"));
+        HashMap<String, Order> map = OrderConvert.process(orders);
+        assertThat(map.get("3sfe"), is(new Order("3sfe", "Shirt")));
+    }
+
+    @Test
+    public void whenDuplicateOrder() {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order("3sfe", "Shirt"));
+        orders.add(new Order("3sfe", "Dress"));
+        orders.add(new Order("3sfe", "Jacket"));
+        HashMap<String, Order> map = OrderConvert.process(orders);
+        assertThat(map.get("3sfe"), is(new Order("3sfe", "Jacket")));
+    }
+
+    @Test
+    public void whenDuplicateOrderThanSize1() {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order("3sfe", "Shirt"));
+        orders.add(new Order("3sfe", "Dress"));
+        orders.add(new Order("3sfe", "Jacket"));
+        HashMap<String, Order> map = OrderConvert.process(orders);
+        assertThat(map.size(), is(1));
+    }
+
+    @Test
+    public void whenSeveralDuplicateOrders() {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order("3sfe", "Shirt"));
+        orders.add(new Order("5sfe", "Shirt"));
+        orders.add(new Order("13sfe", "Shirt"));
+        HashMap<String, Order> map = OrderConvert.process(orders);
+        assertThat(map.get("5sfe"), is(new Order("5sfe", "Shirt")));
+    }
 }
