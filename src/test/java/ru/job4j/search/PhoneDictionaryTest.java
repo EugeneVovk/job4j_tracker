e2d20cd3d366f06ecad542e7578973e317d45dbe
Eugene Vovk <eugenevovk@gmail.com>
2021-11-23 15:25:49 +0300

    1. Телефонный справочник на базе ArrayList [#41598 #228715] added a test, when nothing is found by the given criterion and an empty list is returned.

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -3,7 +3,8 @@ package ru.job4j.search;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.empty;
 import static org.junit.Assert.assertThat;
 
 public class PhoneDictionaryTest {
@@ -16,5 +17,15 @@ public class PhoneDictionaryTest {
         );
         ArrayList<Person> people = phone.find("Petr");
         assertThat(people.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenNotFind() {
+        PhoneDictionary phone = new PhoneDictionary();
+        phone.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> people = phone.find("Anton");
+        assertThat(people, is(empty()));
     }
 }
