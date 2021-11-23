31b5013a608d807b125932e1d2304fc1244329cb
Eugene Vovk <eugenevovk@gmail.com>
2021-11-23 17:12:54 +0300

    0. Map, HashMap [#212652 #230871] corrected the code

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -1,27 +1,11 @@
 package ru.job4j.collection;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
-        /*Вставить данные в карту можно используя метод put.*/
-        map.put("parsentev@yandex.ru", "Petr Arsentev");
-        /*Обновление пары.*/
         map.put("parsentev@yandex.ru", "Petr Arsentev Sergeevich");
-        /*Удаление происходит по ключу через метод remove(key)*/
-        map.remove("parsentev@yandex.ru");
-        /*Вывести все элементы на печать или итерироваться по элементам.*/
-        for (String key : map.keySet()) {
-            String value = map.get(key);
-            System.out.println(key + " = " + value);
-        }
-        for (Map.Entry<String, String> entry : map.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(key + " = " + value);
-        }
         map.put("eugenevovk@gmail.com", "Vovk Evgeniy Nokolaevich");
         for (String key : map.keySet()) {
             String value = map.get(key);
