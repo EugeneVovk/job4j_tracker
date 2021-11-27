915072a9c2d15979aed2cea124d3c7def50ac4c4
Eugene Vovk <eugenevovk@gmail.com>
2021-11-28 02:09:04 +0300

    6. Функции высшего порядка [#24255]

--- src/main/java/ru/job4j/collection/JobSorter.java
+++ src/main/java/ru/job4j/collection/JobSorter.java
@@ -29,5 +29,23 @@ public class JobSorter {
                 .thenComparing(new JobDescByName())
                 .thenComparing(new JobDescByPriority());
         jobs.sort(comb);
+
+        List<Job> jobs2 = Arrays.asList(
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2),
+                new Job("X bug", 0)
+        );
+        jobs2.sort(new JobDescByName().thenComparing(new JobDescByPriority()));
+        System.out.println(jobs2);
+
+        Comparator<Job> compareName = Comparator.comparing(Job::getName);
+        Comparator<Job> comparePriority = Comparator.comparingInt(Job::getPriority);
+        /*Объект combine - это функция высшего порядка.
+         Функции высшего порядка - это функции, зависящие от других функций.*/
+        Comparator<Job> combine = compareName.thenComparing(comparePriority);
+
+        jobs2.sort(combine);
+        System.out.println(jobs2);
     }
 }
