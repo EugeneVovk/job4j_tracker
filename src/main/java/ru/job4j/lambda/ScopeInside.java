a44371d2ac649b75097c42d14039d8b7ce479a9a
Eugene Vovk <eugenevovk@gmail.com>
2021-11-28 14:28:08 +0300

    4. Зона видимости в лямбда-выражениях и исключения [#70609 #233517] corrected the array element counting

--- src/main/java/ru/job4j/lambda/ScopeInside.java
+++ src/main/java/ru/job4j/lambda/ScopeInside.java
@@ -9,8 +9,7 @@ public class ScopeInside {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3};
         int total = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            int num = i;
+        for (int num : numbers) {
             int finalTotal = total;
             total = add(
                     () -> finalTotal + num
