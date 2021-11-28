8a386d425344f2642eeb9463f1569a554cd176cd
Eugene Vovk <eugenevovk@gmail.com>
2021-11-28 14:47:44 +0300

    0.1. Отладка лямбда в IDEA [#259165]

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -1,7 +1,6 @@
 package ru.job4j.stream;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StreamUsage {
 
@@ -13,46 +12,18 @@ public class StreamUsage {
             this.name = name;
             this.spent = spent;
         }
-
-        @Override
-        public String toString() {
-            return "Task{"
-                    + "name='" + name + '\''
-                    + ", spent=" + spent
-                    + '}';
-        }
     }
 
-    /**
-     * Отфильтруем все задачи, где в имени есть слово BUG и сохраним ее в коллекцию List.
-     */
     public static void main(String[] args) {
         List<Task> tasks = List.of(
-                new Task("Bug #1", 100),
-                new Task("Task #2", 100),
-                new Task("Bug #3", 100)
+                new Task("Bug #1", 10),
+                new Task("Task #2", 20),
+                new Task("Bug #3", 40)
         );
-        /* Мы получаем из коллекции Stream или поток элементов.
-        Каждый элемент передается в метод filter().
-        Метод filter() принимает объект Predicate<Task>.
-        Если метод возвращает true, то элемент передается дальше.
-        .collect позволяет полученный результат сохранить в коллекции типа List.
-         */
-        List<Task> bugs = tasks.stream()
+        tasks.stream()
                 .filter(task -> task.name.contains("Bug"))
-                .collect(Collectors.toList());
-        bugs.forEach(System.out::println);
-        /*Допустим, что нам нужно получить только имена задач.
-        Для этого нужно применить метод map().*/
-        List<String> names = tasks.stream()
-                .map(task -> task.name)
-                .collect(Collectors.toList());
-        names.forEach(System.out::println);
-        /*Давайте теперь посчитаем общее время, потраченное на все задачи.
-          Каждое значение task.spent складывает с начальным значением - 0L.*/
-        long total = tasks.stream()
-                .map(task -> task.spent)
-                .reduce(0L, Long::sum);
-        System.out.println(total);
+                .filter(task -> task.spent > 30)
+                .map(task -> task.name + " " + task.spent)
+                .forEach(System.out::println);
     }
 }
