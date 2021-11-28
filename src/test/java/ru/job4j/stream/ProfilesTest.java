b2cf8ec260511aad4bd14bf47b0e640301258f86
Eugene Vovk <eugenevovk@gmail.com>
2021-11-28 17:45:09 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -25,4 +25,23 @@ public class ProfilesTest {
         assertEquals(expected, rsl);
     }
 
+    @Test
+    public void cutClone() {
+        List<Profile> clients = List.of(
+                new Profile(new Address("Big city", "Big street", 10, 723)),
+                new Profile(new Address("Medium city", "Medium street", 2, 20)),
+                new Profile(new Address("Big city", "Big street", 10, 723)),
+                new Profile(new Address("Small city", "Small street", 1, 4)),
+                new Profile(new Address("Big city", "Big street", 10, 722))
+        );
+        Profiles profiles = new Profiles();
+        List<Address> rsl = profiles.collect(clients);
+        List<Address> expected = List.of(
+                new Address("Big city", "Big street", 10, 723),
+                new Address("Big city", "Big street", 10, 722),
+                new Address("Medium city", "Medium street", 2, 20),
+                new Address("Small city", "Small street", 1, 4)
+        );
+        assertEquals(expected, rsl);
+    }
 }
