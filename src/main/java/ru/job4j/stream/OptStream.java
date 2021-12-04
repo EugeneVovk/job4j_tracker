2129c82451adbe66a1750adeb4a37df267724172
Eugene Vovk <eugenevovk@gmail.com>
2021-12-04 22:14:11 +0300

    4. Optional в Stream API. [#279130 #235993]

---
+++ src/main/java/ru/job4j/stream/OptStream.java
@@ -0,0 +1,27 @@
+package ru.job4j.stream;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class OptStream {
+    public static void main(String[] args) {
+        /*
+        Терминальные методы поиска в Stream API возвращают объект Optional.
+        Методы поиска могут вернуть null.
+        C null объектом в потоке данных работать уже нельзя,
+         поэтому все null оборачиваются в Optional.
+        Рассмотрим метод поиска числа в массиве.
+         Метод findFirst вернет первый элемент отфильтрованного потока.
+         Если отфильтрованный поток пустой, то нужно вернуть null.
+         Поэтому здесь используется Optional.*/
+        Stream<Integer> stream = Stream.of(1, 2, 3);
+        Optional<Integer> rsl = stream
+                .filter(e -> e == 5)
+                .findFirst();
+        if (rsl.isPresent()) {
+            System.out.println(rsl.get());
+        } else {
+            System.out.println("Element not found.");
+        }
+    }
+}
