f80ad2202209d9030d50c4ec1d51d7259292d969
Eugene Vovk <eugenevovk@gmail.com>
2021-12-04 19:43:04 +0300

    2. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -1,9 +1,6 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Создание справочника подразделений
@@ -12,33 +9,24 @@ import java.util.Set;
  */
 public class Departments {
 
-    /**
-     * Чтобы реализовать метод fillGaps, нужно определить, какие элементы отсутствуют в системе.
-     * Для этого каждую входящую строку нужно разбить на одиночные элементы.
-     * Для этого можно использовать метод String.split("/").
-     * Этот метод вернет массив одиночных элементов.
-     * Далее нужно через цикл последовательно складывать элементы и добавлять их в множество.
-     * Используйте в качестве промежуточного хранения LinkedHashSet.
-     *
-     * @param deps
-     * @return
-     */
     public static List<String> fillGaps(List<String> deps) {
         Set<String> tmp = new LinkedHashSet<>();
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                tmp.add(start + "/" + el);
+                start += el;
+                tmp.add(start);
+                start += "/";
             }
         }
         return new ArrayList<>(tmp);
     }
 
     public static void sortAsc(List<String> orgs) {
-
+        Collections.sort(orgs);
     }
 
     public static void sortDesc(List<String> orgs) {
-
+        orgs.sort(new DepDescComp());
     }
 }
