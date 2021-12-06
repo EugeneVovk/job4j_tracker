3de6708b8696fea980f14ab9d806a6529827b785
Eugene Vovk <eugenevovk@gmail.com>
2021-12-07 00:05:15 +0300

    1.9. Удаление пар ключ-значение из отображения

---
+++ src/main/java/ru/job4j/map/NonUniqueString.java
@@ -0,0 +1,25 @@
+package ru.job4j.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Метод принимает массив строк, в котором строки могут повторяться.
+ * Необходимо перебрать массив строк и сложить их в карту.
+ * При этом должны выполниться следующие условия:
+ * 1. Ключом является строка.
+ * 2. Значением будет булево переменная.
+ * 3. Если строка встречается в массиве 2 и более раз,
+ * то булево значение должно быть true.
+ * В ином случае - должно принимать значение false.
+ */
+public class NonUniqueString {
+    public static Map<String, Boolean> checkData(String[] strings) {
+        Map<String, Boolean> map = new HashMap<>();
+        for (String str : strings) {
+            map.computeIfPresent(str, (k, v) -> true);
+            map.putIfAbsent(str, false);
+        }
+        return map;
+    }
+}
