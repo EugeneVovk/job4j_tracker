0871e2609949ac6f8a626f33b9dbb07be9ee4ba5
Eugene Vovk <eugenevovk@gmail.com>
2022-01-29 00:39:26 +0300

    6.1. Промежуточные операции. Метод filter(). Фильтрация чисел

---
+++ src/main/java/ru/job4j/stream/FilterNumbers.java
@@ -0,0 +1,22 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 6.1. Промежуточные операции. Метод filter(). Фильтрация чисел
+ * Метод filter() используется чаще всего.
+ * Нужен он чтобы оставить в стриме только объекты, которые удовлетворяют условию
+ * Например,
+ * List.of(-1, 0, 1, 2).stream().filter(n -> n > 0);
+ * - оставить в стриме только положительные числа
+ * <p>
+ * Ваша задача отфильтровать список, оставив в нем только четные числа
+ */
+public class FilterNumbers {
+    public static List<Integer> filter(List<Integer> data) {
+        return data.stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+    }
+}
