c5efd0559613be94e2158ce4b495b3c4dc07cf7c
Eugene Vovk <eugenevovk@gmail.com>
2022-01-28 22:36:54 +0300

    5.5. Промежуточные операции. Метод mapToInt(). Сумма кодов символов

---
+++ src/main/java/ru/job4j/stream/MapToIntMethod.java
@@ -0,0 +1,26 @@
+package ru.job4j.stream;
+
+
+import java.util.List;
+
+/**
+ * 5.5. Промежуточные операции. Метод mapToInt().
+ * Сумма кодов символов.
+ * Стримы примитивов содержат некоторые полезные терминальные операции.
+ * Иногда может потребоваться вызвать один из методов.
+ * Но что делать, если у нас стрим объектов?
+ * Мы можем преобразовать стрим объектов в стрим примитивов,
+ * используя методы mapToInt(), mapToDouble(), mapToLong().
+ * Соответственно, в IntStream, DoubleStream, LongStream.
+ * Работают эти методы аналогично map()
+ * <p>
+ * Ваша задача преобразовать стрим Character в IntStream,
+ * т.е. в стрим кодов символов, и получить сумму этих кодов
+ * Код символа можно получить так
+ * int code = (int) 'a';
+ */
+public class MapToIntMethod {
+    public static long sum(List<Character> characters) {
+        return characters.stream().mapToInt(c -> (int) c).sum();
+    }
+}
